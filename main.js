7dd4a7b2753bfb33c94d4dd1af141fb3fd988bac
inessorzano <inessorzano@gmail.com>
2023-01-23 23:17:11 +0100

    no se que hago con mi vida

--- main.js
+++ main.js
@@ -4,22 +4,46 @@ let url = "https://pokeapi.co/api/v2/pokemon/"
 let reply = document.getElementById("reply");
 let callAPI = document.getElementById("callAPI");
 let info = document.getElementById("nani");
+let counter = 1;
 
-callAPI = function() {
-  // let name = document.getElementById("name");
-  axios.get(url + "vaporeon")
-  .then(function (response) {
-    // manejar respuesta exitosa
+callAPI = async function() {
+  let name = document.getElementById("name");
+  
+  try {
+    const response = await axios.get(url + name.value);
     console.log(response);
-  })
-  .catch(function (error) {
-    // manejar error
-    console.log(error);
-  })
-  .then(function () {
-    // siempre sera executado
-  });
-  info.innerHTML = "¿Sabías que vaporeon ...."
+  } catch (error) {
+    console.error(error);
+  }
+
+  switch(name.value){
+    case "vaporeon":
+      if(!window.localStorage.getItem("counter")){
+        window.localStorage.setItem("counter",counter);
+      }else{
+        // ParseInt porque el muy troll lo guarda como string
+        window.localStorage.setItem("counter",parseInt(window.localStorage.getItem("counter")) +  counter);
+      }
+      console.log(window.localStorage.getItem("counter"));
+      switch (parseInt(window.localStorage.getItem("counter"))){
+        case 34:
+          console.log("oh no");
+          info.innerHTML = '¿Sabías que vaporeon ... <img id="down" src="caret-down-circle.svg"></img>';
+
+          let app = document.getElementById("app");
+
+          let down = document.getElementById("down");
+          down.style.display="inline";
+          down.onclick = function(){
+            var img = document.createElement("img");
+            img.src="no.png";
+            img.classList.add("center");
+            document.body.appendChild(img);
+            app.style.display="none";
+          };
+
+      }
+  }
 };
 
 window.callAPI = callAPI;  
