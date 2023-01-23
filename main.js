e0229878855e0d12192856503d4cd9bf12504ade
inessorzano <inessorzano@gmail.com>
2023-01-23 21:17:58 +0100

    no creo no

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ let url = "https://pokeapi.co/api/v2/pokemon/"
 
 let reply = document.getElementById("reply");
 let callAPI = document.getElementById("callAPI");
+let info = document.getElementById("nani");
 
 callAPI = function() {
-  let name = document.getElementById("name");
-  axios.get(url + name.value)
+  // let name = document.getElementById("name");
+  axios.get(url + "vaporeon")
   .then(function (response) {
     // manejar respuesta exitosa
     console.log(response);
   })
+  info.innerHTML = "¿Sabías que vaporeon ...."
   
 };
 
